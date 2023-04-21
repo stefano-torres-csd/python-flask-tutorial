f3146dae6d506e2fdb2dda8c53713e30272d5cc4
stefano-torres-csd <108374618+stefano-torres-csd@users.noreply.github.com>
2023-04-21 20:54:59 +0000

    prior to data usage

--- app.py
+++ app.py
@@ -2,6 +2,33 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+ITEMS = [
+  {
+    'id':1,
+    'item_no': 1234,
+    'name': 'Some item',
+    'desc': 'Some sort of description',
+    'cost': 1.34,
+    'price': 1.55
+  },
+  {
+    'id':2,
+    'item_no': 2345,
+    'name': 'item 2',
+    'desc': 'Some sort of description',
+    'cost': 1.34,
+    'price': 1.55
+  },
+  {
+    'id':3,
+    'item_no': 3456,
+    'name': 'item 3',
+    'desc': 'Some sort of description',
+    'cost': 1.34,
+    'price': 1.55
+  }
+]
+
 
 @app.route("/")
 def hello_world():
